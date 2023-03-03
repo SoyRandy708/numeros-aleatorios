092966f89f59de2b0433f433e6d434e3ea8c0def
Randy <randy708ochoa@gmail.com>
2023-03-02 22:03:35 -0600

    Validacion para saber si la cantidad de resultados es mayor a 0 y ahora se pueden mostrar muchas numeros

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,26 @@
 const respuesta = document.getElementById("respuesta")
 const enviar = document.getElementById("enviar")
 
-enviar.addEventListener("click", numerosAleatorio)
+function cantidadNumeros() {
+    let resultados = parseInt(document.getElementById("numero-resultados").value) || 0
 
-function numerosAleatorio() {
+    if(resultados <= 0) {
+        console.log("Debes generar al menos 1 resultado")
+    } else {
+        numerosAleatorio(resultados)
+    }
+}
+
+function numerosAleatorio(ejecuciones) {
     let minimo = parseInt(document.getElementById("numero-menor").value) || 0
     let maximo = parseInt(document.getElementById("numero-mayor").value) || 0
-    let numeroAleatorio = Math.floor(Math.random() * (maximo - minimo + 1)) + minimo
-
-    respuesta.innerText = `Numeros: ${numeroAleatorio}`
+    let numeros = []
+    
+    for (let index = 0; index < ejecuciones; index++) {
+        let numeroAleatorio = Math.floor(Math.random() * (maximo - minimo + 1)) + minimo
+        numeros.push(numeroAleatorio)
+    }
+    respuesta.innerText = `Numeros: ${numeros.join(", ")}`
 }
+
+enviar.addEventListener("click", cantidadNumeros)
