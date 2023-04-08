a0caeaf936e9a421d7fcd42bc2f4580743748e02
Randy <randy708ochoa@gmail.com>
2023-04-08 17:01:37 -0600

    Mejoras del codigo para validar lo que introduce el usuario y poder activar y desactivar funciones

--- src/js/index.js
+++ src/js/index.js
@@ -1,62 +1,69 @@
 const botonResultado = document.getElementById("enviar")
+const numerosRepetidos = document.getElementById("numeros-repetidos")
 const numerosValidacion = document.getElementsByName("numeros-validacion")
 
-function validacion() {
-    const repetirNumeros = document.getElementById("numeros-repetidos")
+const activarBoton = () => {
     let resultados = parseInt(document.getElementById("numero-resultados").value) || 0
-
-    if(resultados === 0) {
+    
+    if(resultados < 1) {
         document.querySelector(".container-numeros-repetidos").classList.add("desactivado")
         botonResultado.disabled = true
-
+        // MOSTRAR MENSAJE DE ERROR
+        
     } else if(resultados === 1) {
         document.querySelector(".container-numeros-repetidos").classList.add("desactivado")
         botonResultado.disabled = false
-        botonResultado.onclick = () => numerosAleatorio(resultados)
-
-    } else if(resultados > 1 && repetirNumeros.checked) {
-        document.querySelector(".container-numeros-repetidos").classList.remove("desactivado")
-        botonResultado.disabled = false
-        botonResultado.onclick = () => numerosAleatorio(resultados)
 
     } else if(resultados > 1) {
         document.querySelector(".container-numeros-repetidos").classList.remove("desactivado")
         botonResultado.disabled = false
-        botonResultado.onclick = () => numerosNoRepetidos(resultados)
 
+    } 
+}
+
+const elegirNumeros = () => {
+    let resultados = parseInt(document.getElementById("numero-resultados").value) || 0
+
+    if(numerosRepetidos.checked) {
+        aleatoriosRepetidos(resultados)
+    } else {
+        aleatoriosNoRepetidos(resultados)
     }
 }
 
-const numerosNoRepetidos = (ejecuciones) => {
+const aleatoriosNoRepetidos = (ejecuciones) => {
     let minimo = parseInt(document.getElementById("numero-menor").value) || 0
     let maximo = parseInt(document.getElementById("numero-mayor").value) || 0
     let numeros = []
 
-    if((maximo - minimo + 1) >= ejecuciones) {
-        botonResultado.disabled = false
-
-        for (let index = 0; index < ejecuciones; index++) {
-            let numeroAleatorio = Math.floor(Math.random() * (maximo - minimo + 1)) + minimo
-    
-            if(!numeros.includes(numeroAleatorio)) {
-                numeros.push(numeroAleatorio)
-            } else {
-                index--
-            }
-        }
-
-        respuesta.innerText = `Numeros: ${numeros.join(", ")}`
-    } else {
+    if((maximo - minimo + 1) < ejecuciones) {
         console.log("El rango de numeros tiene que ser mayor al numero de resultados")
         botonResultado.disabled = true
+        return
+        // MENSAJE DE ERROR 
+    } else if((maximo - minimo + 1) >= ejecuciones) {
+        botonResultado.disabled = false
     }
+    
+    for (let index = 0; index < ejecuciones; index++) {
+        let numeroAleatorio = Math.floor(Math.random() * (maximo - minimo + 1)) + minimo
+
+        if(!numeros.includes(numeroAleatorio)) {
+            numeros.push(numeroAleatorio)
+        } else {
+            index--
+        }
+    }
+
+    respuesta.innerText = `Numeros: ${numeros.join(", ")}`
 }
 
-function numerosAleatorio(ejecuciones) {
+const aleatoriosRepetidos = (ejecuciones) => {
     let minimo = parseInt(document.getElementById("numero-menor").value) || 0
     let maximo = parseInt(document.getElementById("numero-mayor").value) || 0
     let numeros = []
     
+    botonResultado.disabled = false
     for (let index = 0; index < ejecuciones; index++) {
         let numeroAleatorio = Math.floor(Math.random() * (maximo - minimo + 1)) + minimo
         numeros.push(numeroAleatorio)
@@ -65,5 +72,7 @@ function numerosAleatorio(ejecuciones) {
 }
 
 numerosValidacion.forEach((input) => {
-    input.addEventListener('input', validacion)
+    input.addEventListener('input', activarBoton)
 })
+
+botonResultado.addEventListener("click", elegirNumeros)
